5b8ce5a9cb4bd74a7c711db757df64d660892e6a
Paul "Enfield <enfield.0010@gmail.com>
2020-12-14 04:46:10 -0800

    Portfolio category page considered complete.

--- site/templates/portfolioset.php
+++ site/templates/portfolioset.php
@@ -21,6 +21,38 @@ $mainjs = asset('assets/scripts/main.js')->url();
 				alt="">
 			<?= $page->bodycopy()->kirbytext() ?>
 		</article>
+		<div class="worksgrid portfolio">
+			<?php foreach($page->children()->listed()->sortBy($field = 'completiondate', $direction = 'desc') as $piece): ?>
+				<?php if($piece->file('rclmexception.png')): ?>
+					<?php $herothumb = $piece->file('rclmexception.png')->url() ?>
+				<?php else: ?>
+					<?php $herothumb = $piece->heroimg()->
+					toFile()->
+					thumb([
+						'width'   => 800,
+						'height'  => 400,
+						'quality' => 100
+					])->url();?>
+				<?php endif ?>
+				<?php if($piece->file('enfieldhero.jpg')):
+					$buttonalign = 'background-position: initial;' ?>
+				<?php else: 
+					$buttonalign = 'background-position: center;' ?>
+				<?php endif ?>
+				<a href="<?= $piece->url() ?>" style="
+					background-image: url('<?= $herothumb ?>');
+					<?= $buttonalign ?>
+				">
+					<div>
+						<div><?= $piece->completiondate()->toDate('Y') ?></div>
+						<div>
+							<h3><?= $piece->title() ?></h3>
+							<p><?= $piece->synopsis() ?></p>
+						</div>
+					</div>
+				</a>
+			<?php endforeach ?>
+		</div>
 	</div>
 </main>
 
